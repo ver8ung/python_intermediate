e6d0554f4f78197c5397ca1c095cc01841fa7161
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-21 16:37:46 +0100

    Create custom_iterator2.py

---
+++ custom_iterator2.py
@@ -0,0 +1,18 @@
+#iterator incorporation a loop
+class Zähler:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 100:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+zähler = Zähler()
+iterate = iter(zähler)
+for x in iterate:
+    print (x)
