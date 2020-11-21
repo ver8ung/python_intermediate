7abc1438e2bfdeaab21ffc2f5f10022e1c244a82
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-21 16:17:21 +0100

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -7,12 +7,24 @@ print(next(testiterator))
 print(next(testiterator))
 print(next(testiterator))
 
-#iterate sequence of characters
+#iterate over sequence of characters
 teststring = "lemon"
 testiterator2 = iter(teststring)
 
-print(next(teststring))
-print(next(teststring))
-print(next(teststring))
-print(next(teststring))
-print(next(teststring))
+print(next(testiterator2))
+print(next(testiterator2))
+print(next(testiterator2))
+print(next(testiterator2))
+print(next(testiterator2))
+
+#looping over a tuple
+testtuple2 = ("banana", "lemon", "apple")
+
+for i in testtuple2:
+    print(i)
+
+#looping over a string
+teststring2 = "banana"
+
+for i in teststring2:
+    print(i)
