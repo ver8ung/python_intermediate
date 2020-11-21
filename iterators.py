dc956b0747265c2cecd546d8a7743db300d5c98a
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-21 16:11:06 +0100

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,7 +1,18 @@
 # Iterators
+# iterate over items in tuple
 testtuple = ("banana", "lemon", "apple")
 testiterator = iter(testtuple)
 
 print(next(testiterator))
 print(next(testiterator))
 print(next(testiterator))
+
+#iterate sequence of characters
+teststring = "lemon"
+testiterator2 = iter(teststring)
+
+print(next(teststring))
+print(next(teststring))
+print(next(teststring))
+print(next(teststring))
+print(next(teststring))
