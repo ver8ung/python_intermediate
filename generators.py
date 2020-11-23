24281e3dfcea9d9724a6b270b3cdc4745f37c10d
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-23 14:50:32 +0100

    Update generators.py

--- generators.py
+++ generators.py
@@ -15,4 +15,33 @@ print(next(newGenerator))
 print(next(newGenerator))
 print(next(newGenerator))
 
-print(next(newGenerator)) #raises StopIteration
+# print(next(newGenerator)) a 4th time creates StopIteration
+# using return inside the generator function terminates it after return is called
+
+#generator with for loop
+
+def Upto(x):
+    for i in range(x):
+        yield i
+
+seq1 = Upto(7)
+while True:
+    try:
+        print("The number is:", next(seq1))
+    except StopIteration:
+        break
+#print(next(seq1))
+
+
+# yield square of number#
+def squareSeq(x):
+    for i in range(x):
+        yield i * i
+
+newGenerator2 = squareSeq(7)
+
+while True:
+    try:
+        print("Recieved on the next(): ", next(newGenerator2))
+    except StopIteration:
+        break
