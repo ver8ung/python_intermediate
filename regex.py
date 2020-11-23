f0fbfa237ca1e4c10a03ed1869c5b2a2db63d9e7
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-23 16:09:58 +0100

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,2 +1,12 @@
 # regular expressions in python (RegEx)
 import re
+
+# search string for match or no match
+text = "You can start learning Python."
+x = re.search("^You.*Python.$", text) #^You. = string has to begin with You
+                                      # *Python.$ = string has to end with Python.
+
+if x:
+    print("Yes, we have a match!")
+else:
+    print("No match!")
