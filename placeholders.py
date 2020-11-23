8938596e8a2b184e8521ad5e418c0fec79a43db4
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-23 14:08:28 +0100

    Update placeholders.py

--- placeholders.py
+++ placeholders.py
@@ -7,4 +7,11 @@ newmsg = "The number is {} for you!"
 print(newmsg.format(number))
 print(newmsg.format(word))
 
-#
+#print number with 2 decimals .2f
+
+number = 25
+newmsg2 = "Your number is {:.2f}" #:=string
+
+print(newmsg2.format(number))
+
+#adding multiple placeholders
