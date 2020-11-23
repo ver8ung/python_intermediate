5d474a1cb1a245cb91f9838b0655634e3e02cf4b
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-23 14:13:38 +0100

    Update placeholders.py

--- placeholders.py
+++ placeholders.py
@@ -10,8 +10,22 @@ print(newmsg.format(word))
 #print number with 2 decimals .2f
 
 number = 25
-newmsg2 = "Your number is {:.2f}" #:=string
+newmsg2 = "Your number is {:.2f}" #:=after string
 
 print(newmsg2.format(number))
 
 #adding multiple placeholders
+quantity = 5
+itemNumber = 50
+price = 24
+firstorder = "We want {} pieces of item number {} for {:.2f} Euros."
+
+print(firstorder.format(quantity, itemNumber, price))
+
+#using index numbers
+
+quantity = 7 # = index number 0
+itemNumber = 77
+price = 23.55
+secondorder = "We want {0} pieces of item number {1} for {2:.2f} Euros."
+print(secondorder.format(quantity, itemNumber, price))
