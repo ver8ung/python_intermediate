1a7f09ded8bd6bf693701b177c87bd61242c1992
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-24 10:52:22 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -15,3 +15,23 @@ import sys
 sys.maxsize
 sys.path
 sys.version
+
+#math module loads popular mathematical functions
+import math
+math.pi
+math.e
+math.radians(20)
+math.degrees(math.pi/5)
+math.sin(30)
+math.cos(40)
+math.tan(0.5)
+math.log(20)
+math.log10(15)
+math.exp(2)
+math.pow(2, 3)
+math.sqrt(100)
+math.ceil(5.59)
+math.floor(5.59)
+
+#statistics module
+import stats
