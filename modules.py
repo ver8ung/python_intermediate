2861971fe0a9a948ebcc8fc6691c5c34852c6469
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-24 11:03:40 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -12,12 +12,14 @@ os.listdir ("F:\\tempdir")
 
 # sys module
 import sys
+
 sys.maxsize
 sys.path
 sys.version
 
 #math module loads popular mathematical functions
 import math
+
 math.pi
 math.e
 math.radians(20)
@@ -34,4 +36,33 @@ math.ceil(5.59)
 math.floor(5.59)
 
 #statistics module
-import stats
+import statistics
+
+statistics.mean ([3, 5, 7, 10])
+statistics.median ([3, 5, 7, 10])
+statistics.mode ([3, 5, 7, 7, 10]) #most common value
+
+#collections and namedtuple
+import collections
+player = collections.namedtuple ("player", ["name", "age", "team"])
+p1 = player ("Salah", 27, "Egypt")
+print(p1.name)
+print(p1.age)
+print.(p1.team)
+
+#collections and ordered dict
+dict1 = collections.orderedDict()
+dict1["First:"] = 35
+dict1["Second:"] = 40
+dict1["Third:"] = 45
+dict1["Fourth:"] = 50
+for k,v in dict.items(): # k=key v=value
+           print(k,v)
+    
+#deque function
+dq = collections.deque ([20, 25, 30, 35])
+dq.appendleft(15)
+dq.appendright(40)
+q.pop()
+q.popleft
+q.popright
