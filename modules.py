21e5d39298001c999110b1c1bc8510f246314b91
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-24 10:42:58 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -3,3 +3,9 @@ import os # modules to interact with operating system
 os.mkdir("F:\\tempdir")
 os.chdir("F:\\tempdir")
 os.getcwd()
+os.chdir("F:\\tempdir")
+x = os.getcwd()
+print(x)
+
+os.rmdir("F:\\tempdir")
+os.listdir ("F:\\tempdir")
