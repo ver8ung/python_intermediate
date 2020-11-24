a6975c2f6f9b4696f46901a61188a85695620376
ver8ung <45457029+ver8ung@users.noreply.github.com>
2020-11-24 10:39:34 +0100

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,5 @@
+# Python built-in modules (predefined functions that can be used globally, written in C)
+import os # modules to interact with operating system
+os.mkdir("F:\\tempdir")
+os.chdir("F:\\tempdir")
+os.getcwd()
